17dfb12654be00b31801c00503e631d25564e51d
IvyJeptoo <ivy.jeptoo@student.moringaschool.com>
2022-02-06 20:01:57 +0300

    patch: fixed functionality

--- js/script.js
+++ js/script.js
@@ -1,79 +1,32 @@
-function myScoreChecker(){  
+function myScoreChecker(){
     
-var score = 0;
-const correctAnswers = ["10 years","Tea","Lake Victoria", "8","Tsavo NP","1963"];
-var questionOne = document.getElementByName('quiz1');
-for ( var i = 0, length  = questionOne.length; i < length; i++){
-    if(questionOne[i].checked && questionOne[i] === correctAnswers[0]){
-        score += 10;   
-         
+  var score=0;
+  var rightAnswer1= document.questions.quiz1.value;
+  var rightAnswer2= document.questions.quiz2.value;
+  var rightAnswer3= document.questions.quiz3.value;
+  var rightAnswer4= document.questions.quiz4.value;
+  var rightAnswer5= document.questions.quiz5.value;
+  var rightAnswer6= document.questions.quiz6.value;
+  var answer=document.getElementById("answer")
+
+  if(rightAnswer1==="10 years"){score ++};
+  if(rightAnswer2==="Tea"){score ++};
+  if(rightAnswer3==="Lake Victoria"){score ++};
+  if(rightAnswer4==="8"){score ++};
+  if(rightAnswer5==="Tsavo NP"){score ++};
+  if(rightAnswer6==="1963"){score ++};
+
+  questions.style.display = "none"
+  
+
+  if (score >= 5) {
+        answer.textContent="CONGRATS! A TRUE PATRIOT! Your score is "+ score/6*100 + "% "
     
-        break;
-        }
-    }
-        var questionTwo = document.getElementByName('quiz2');
-   for ( var i = 0, length  = questionTwo.length; i < length; i++){
-    if(questionTwo[i].checked && questionTwo[i] === correctAnswers[1]){
-        score += 10;   
-         
-   
-        break;
-        }
-    }
-        var questionThree = document.getElementByName('quiz3');
-        for ( var i = 0, length  = questionThree.length; i < length; i++){
-         if(questionThree[i].checked && questionThree[i] === correctAnswers[2]){
-             score += 10;   
-              
-        
-             break;
-             }
-            }
-             var questionFour = document.getElementByName('quiz4');
-             for ( var i = 0, length  = questionFour.length; i < length; i++){
-              if(questionFour[i].checked && questionFour[i] === correctAnswers[3]){
-                  score += 10;   
-                   
-              
-                  break;
-                  }  
-                }   
-                  var questionFive = document.getElementByName('quiz5');
-                  for ( var i = 0, length  = questionFive.length; i < length; i++){
-                   if(questionFive[i].checked && questionFive[i] === correctAnswers[4]){
-                       score += 10;   
-                        
-                    
-                       break;
-                       }   
-                    }   
-                       var questionSix = document.getElementByName('quiz6');
-                       for ( var i = 0, length  = questionSix.length; i < length; i++){
-                        if(questionSix[i].checked && questionSix[i] === correctAnswers[5]){
-                            score += 10;   
-                             
-                        
-                            break;
-                            }  
-                        }   
+          }else if(score >= 3){
+              answer.textContent="GOOD! Your score is"+ score/6*100 + "%. Fair amount of information."
+          }else{
+              answer.textContent="PULL UP! Your score is"+ score/6*100 + "%. So much much you need to know about Kenya!"
+          }
+  
+  }
 
-                        
-
-                    
-document.getElementById("answer").textContent = score;
- }
-
- function compareData(score) {
-      if (score==60 || 50) {
-          answer.textContent="CONGRATS! Your score is "+ score/6*100 + "%. A TRUE PATRIOT! "
-
-      }else if(score==40 || 30){
-          answer.textContent="GOOD! Your score is"+ score/6*100 + "%. Fair amount of information."
-      }else{
-          answer.textContent="PULL UP! Your score is"+ score/6*100 + "%. So much much you need to know about Kenya!"
-      }
-        
-    
-
-    
- }
