f645013c0ca3132f120fdcb64b48e06304e0499d
IvyJeptoo <ivy.jeptoo@student.moringaschool.com>
2022-02-06 14:35:03 +0300

    style:call function to display score

---
+++ js/script.js
@@ -0,0 +1,67 @@
+function myScoreChecker(e){  
+    e.preventDefault();  
+var score = 0;
+const correctAnswers = ["10 years","Tea","Lake Victoria", "8","Tsavo NP","1963"];
+var questionOne = document.getElementByName('quiz1');
+for ( var i = 0, length  = questionOne.length; i < length; i++){
+    if(questionOne[i].checked && questionOne[i] === correctAnswers[0]){
+        score += 10;   
+         
+    
+        break;
+        }
+        var questionOne = document.getElementByName('quiz2');
+   for ( var i = 0, length  = questionOne.length; i < length; i++){
+    if(questionOne[i].checked && questionOne[i] === correctAnswers[1]){
+        score += 10;   
+         
+   
+        break;
+        }
+        var questionOne = document.getElementByName('quiz3');
+        for ( var i = 0, length  = questionOne.length; i < length; i++){
+         if(questionOne[i].checked && questionOne[i] === correctAnswers[2]){
+             score += 10;   
+              
+        
+             break;
+             }
+             var questionOne = document.getElementByName('quiz4');
+             for ( var i = 0, length  = questionOne.length; i < length; i++){
+              if(questionOne[i].checked && questionOne[i] === correctAnswers[3]){
+                  score += 10;   
+                   
+              
+                  break;
+                  }     
+                  var questionOne = document.getElementByName('quiz5');
+                  for ( var i = 0, length  = questionOne.length; i < length; i++){
+                   if(questionOne[i].checked && questionOne[i] === correctAnswers[4]){
+                       score += 10;   
+                        
+                    
+                       break;
+                       }      
+                       var questionOne = document.getElementByName('quiz6');
+                       for ( var i = 0, length  = questionOne.length; i < length; i++){
+                        if(questionOne[i].checked && questionOne[i] === correctAnswers[5]){
+                            score += 10;   
+                             
+                        
+                            break;
+                            }     
+
+                        }
+
+
+document.getElementById("results").innerHTML = score;
+}
+
+
+
+
+
+
+
+
+
